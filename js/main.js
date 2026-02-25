3faa4f41a327c3eaeebbd46f82d161af50938e02
Mykhailo Hakman <88773323+deorfeal@users.noreply.github.com>
2026-02-25 22:19:33 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-// document.querySelectorAll('.faq-list__item').forEach(item => {
-//     item.addEventListener('click', () => {
-//         // Если элемент уже активен, убрать активный класс
-//         if (item.classList.contains('faq-list__item--active')) {
-//             item.classList.remove('faq-list__item--active');
-//         } else {
-//             // Убираем класс у всех элементов
-//             document.querySelectorAll('.faq-list__item').forEach(el => el.classList.remove('faq-list__item--active'));
-//             // Добавляем класс текущему элементу
-//             item.classList.add('faq-list__item--active');
-//         }
-//     });
-// });
-
-//
-
 // Функция активации
 function activateBody() {
   document.body.classList.add("body--actived");
@@ -701,6 +685,7 @@ $(function () {
       details: $(".popup--details"),
       languages: $(".popup--languages"),
       back: $(".popup--back"),
+      passage: $(".popup--passage"),
     };
 
     function showPopup($popupToShow) {
@@ -746,13 +731,17 @@ $(function () {
       showPopup($popups.languages);
     });
 
+     $(".qrs-item__img").click(function (event) {
+      event.stopPropagation();
+      event.preventDefault();
+      showPopup($popups.passage);
+    });
 
     $(".bottom__link--details").click(function (event) {
       event.stopPropagation();
       event.preventDefault();
       showPopup($popups.details);
     });
-
 
     // Клик на айтем основного слайдера — открываем попап и переходим к нужному слайду
     $(".presentation-item").click(function (event) {
@@ -807,200 +796,3 @@ $(function () {
     });
   });
 });
-
-// Aos - the right initialisation
-// jQuery(document).ready(function () {
-//     (function () {
-//         // your page initialization code here
-//         // the DOM will be available here
-//         AOS.init({
-//             duration: 1000,
-//             offset: 0, // offset (in px) from the original trigger point
-//             anchorPlacement: 'top-bottom', // define where the AOS animations will be triggered
-//         });
-//     })();
-// });
-// //
-
-// timer
-// setInterval(() => {
-//     let timeNow = new Date() // Время сейчас
-//     let timeNowTimeStamp = timeNow.getTime() // сколько прошоло с 1970 до теперешнего момената
-//     // console.log(timeNowTimeStamp)
-
-//     let ourDate = new Date('2023-03-19T23:14:00') // Время нашего знакомства
-//     let ourDateTimeStamp = ourDate.getTime() // сколько прошоло с 1970 до теперешнего момената
-//     // console.log(ourDateTimeStamp)
-
-//     let timeStampMilliseconds = ourDateTimeStamp - timeNowTimeStamp // Миллисекунды ( разница между временем теперь и временем нашей встречи )
-
-//     let secondsOfTimeStamp = timeStampMilliseconds / 1000 // Секунды - разницы
-//     let minutesOfTimeStamp = secondsOfTimeStamp / 60 // минуты - разницы
-//     let hoursOfTimeStamp = minutesOfTimeStamp / 60 // часы - разницы
-//     let daysOfTimeStamp = hoursOfTimeStamp / 24 // Дни - разницы
-
-//     let secondsOfTimeStampFloor = Math.floor(timeStampMilliseconds / 1000) // Секунды - разницы
-//     let minutesOfTimeStampFloor = Math.floor(secondsOfTimeStamp / 60) // минуты - разницы
-//     let secondsRamnant = secondsOfTimeStampFloor - (minutesOfTimeStampFloor * 60) // Остаток секунд - то есть наши секунды в Html
-//     let hoursOfTimeStampFloor = Math.floor(minutesOfTimeStamp / 60) // часы - разницы
-//     let minutesRamnant = minutesOfTimeStampFloor - (hoursOfTimeStampFloor * 60) // Остаток минут - то есть наши минуты в Html
-//     let daysOfTimeStampFloor = Math.floor(hoursOfTimeStamp / 24) // Дни - разницы
-//     let hoursRamnant = hoursOfTimeStampFloor - (daysOfTimeStampFloor * 24) // Остаток часов - то есть наши часы в Html
-
-//     let hours = document.querySelector('.header-bottom-body-row__item-text--hours')
-
-//     let minutes = document.querySelector('.header-bottom-body-row__item-text--minutes')
-
-//     let seconds = document.querySelector('.header-bottom-body-row__item-text--seconds')
-
-//     // // //
-
-//     seconds.innerHTML = secondsRamnant
-
-//     if (hoursRamnant < 10) {
-//         console.log(String(hoursRamnant)[0])
-//         hours.innerHTML = '0' + String(hoursRamnant) + '<span>:</span>'
-//     } else {
-//         hours.innerHTML = String(hoursRamnant) + '<span>:</span>'
-//     }
-
-//     if (minutesRamnant < 10) {
-//         minutes.innerHTML = '0' + String(minutesRamnant) + '<span>:</span>'
-//     } else {
-//         minutes.innerHTML = String(minutesRamnant) + '<span>:</span>'
-//     }
-
-//     if (secondsRamnant < 10) {
-//         seconds.innerHTML = '0' + String(secondsRamnant)
-//     } else {
-//         seconds.innerHTML = String(secondsRamnant)
-//     }
-// }, 1000)
-// //
-
-// typed js
-
-// $(".typed").typed({
-//     strings: ["Графічним дизайнерам", "Початковим веб-дизайнерам", "Студентам/школярам", "Офісним працівникам"],
-//     // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
-//     stringsElement: null,
-//     // typing speed
-//     typeSpeed: 30,
-//     // time before typing starts
-//     startDelay: 1200,
-//     // backspacing speed
-//     backSpeed: 20,
-//     // time before backspacing
-//     backDelay: 500,
-//     // loop
-//     loop: true,
-//     // false = infinite
-//     loopCount: 5,
-//     // show cursor
-//     showCursor: false,
-//     // character for cursor
-//     cursorChar: "|",
-//     // attribute to type (null == text)
-//     attr: null,
-//     // either html or text
-//     contentType: 'html',
-//     // call when done callback function
-//     callback: function () {},
-//     // starting callback function before each string
-//     preStringTyped: function () {},
-//     //callback for every typed string
-//     onStringTyped: function () {},
-//     // callback for reset
-//     resetCallback: function () {}
-// });
-// //
-
-// Phone mask
-
-// window.addEventListener("DOMContentLoaded", function () {
-//     [].forEach.call(document.querySelectorAll('.tel'), function (input) {
-//         var keyCode;
-
-//         function mask(event) {
-//             event.keyCode && (keyCode = event.keyCode);
-//             var pos = this.selectionStart;
-//             if (pos < 3) event.preventDefault();
-//             var matrix = "+7 (___) ___-____",
-//                 i = 0,
-//                 def = matrix.replace(/\D/g, ""),
-//                 val = this.value.replace(/\D/g, ""),
-//                 new_value = matrix.replace(/[_\d]/g, function (a) {
-//                     return i < val.length ? val.charAt(i++) || def.charAt(i) : a
-//                 });
-//             i = new_value.indexOf("_");
-//             if (i != -1) {
-//                 i < 5 && (i = 3);
-//                 new_value = new_value.slice(0, i)
-//             }
-//             var reg = matrix.substr(0, this.value.length).replace(/_+/g,
-//                 function (a) {
-//                     return "\\d{1," + a.length + "}"
-//                 }).replace(/[+()]/g, "\\$&");
-//             reg = new RegExp("^" + reg + "$");
-//             if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
-//             if (event.type == "blur" && this.value.length < 5) this.value = ""
-//         }
-
-//         input.addEventListener("input", mask, false);
-//         input.addEventListener("focus", mask, false);
-//         input.addEventListener("blur", mask, false);
-//         input.addEventListener("keydown", mask, false)
-
-//     });
-
-// });
-
-// //
-
-// Visibilyto of element on scroll or not
-// $(function () {
-//     // Получаем нужный элемент
-//     var element = document.querySelector('footer');
-
-//     var Visible = function (target) {
-//         // let headerBottom = document.querySelectorAll('.header-bottom')[0]
-//         // var menuBtn = document.querySelectorAll('.menu-btn')[0]
-//         // var footerTop = document.querySelectorAll('.footer-top')[0]
-//         // Все позиции элемента
-//         var targetPosition = {
-//                 top: window.pageYOffset + target.getBoundingClientRect().top,
-//                 left: window.pageXOffset + target.getBoundingClientRect().left,
-//                 right: window.pageXOffset + target.getBoundingClientRect().right,
-//                 bottom: window.pageYOffset + target.getBoundingClientRect().bottom
-//             },
-//             // Получаем позиции окна
-//             windowPosition = {
-//                 top: window.pageYOffset,
-//                 left: window.pageXOffset,
-//                 right: window.pageXOffset + document.documentElement.clientWidth,
-//                 bottom: window.pageYOffset + document.documentElement.clientHeight
-//             };
-
-//         if (targetPosition.bottom > windowPosition.top && // Если позиция нижней части элемента больше позиции верхней чайти окна, то элемент виден сверху
-//             targetPosition.top < windowPosition.bottom && // Если позиция верхней части элемента меньше позиции нижней чайти окна, то элемент виден снизу
-//             targetPosition.right > windowPosition.left && // Если позиция правой стороны элемента больше позиции левой части окна, то элемент виден слева
-//             targetPosition.left < windowPosition.right) { // Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
-//             // Если элемент полностью видно, то запускаем следующий код
-//             $('.connection__bottom-btn').addClass('connection__bottom-btn--none')
-//             $('.connection').addClass('connection-margin')
-
-//         } else {
-//             $('.connection__bottom-btn').removeClass('connection__bottom-btn--none')
-//             $('.connection').removeClass('connection-margin')
-//         };
-//     };
-
-//     // Запускаем функцию при прокрутке страницы
-//     window.addEventListener('scroll', function () {
-//         Visible(element);
-//     });
-
-//     // А также запустим функцию сразу. А то вдруг, элемент изначально видно
-//     Visible(element);
-// })
-// //
